0b08f8348f1ff61bda9068323603af576b4e440c
Programmer Develops <74005248+Programmer-Develops@users.noreply.github.com>
2024-07-29 23:04:31 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,5 +1,20 @@
-AFRAME.registerComponent("Main-page",{
+AFRAME.registerComponent("cursor-handler",{
     init:function(){
+        plane=document.querySelector("#bbrain")
+        console.log(plane)
+        plane.addEventListener("click",()=>{
+    
+            body=document.querySelector("#body")
+            console.log(body)
+            pos=body.getAttribute("position")
+            body.setAttribute("visible",false)
+            document.querySelector("#Dbrain").position=pos
+            document.querySelector("#Dbrain").visible="true"
+
+
+
+
+        })
         
     }
 })
